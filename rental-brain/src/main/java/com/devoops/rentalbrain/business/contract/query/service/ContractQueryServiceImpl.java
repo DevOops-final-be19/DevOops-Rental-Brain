6a4b51ad7189f36fcfdb8d5fda6ca70c26821cff
kyuhon <kyuhon123@gmail.com>
2025-12-12 17:43:26 +0900

    feat: #20 계약 리스트 조회기능 미완성

---
+++ rental-brain/src/main/java/com/devoops/rentalbrain/business/contract/query/service/ContractQueryServiceImpl.java
@@ -0,0 +1,38 @@
+package com.devoops.rentalbrain.business.contract.query.service;
+
+import com.devoops.rentalbrain.business.contract.query.dto.AllContractDTO;
+import com.devoops.rentalbrain.business.contract.query.dto.ContractSearchDTO;
+import com.devoops.rentalbrain.business.contract.query.mapper.ContractQueryMapper;
+import com.devoops.rentalbrain.common.Pagination.PageResponseDTO;
+import com.devoops.rentalbrain.common.Pagination.Pagination;
+import com.devoops.rentalbrain.common.Pagination.PagingButtonInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContractQueryServiceImpl implements ContractQueryService {
+
+    private final ContractQueryMapper contractQueryMapper;
+
+    @Autowired
+    public ContractQueryServiceImpl(ContractQueryMapper contractQueryMapper) {
+        this.contractQueryMapper = contractQueryMapper;
+    }
+
+    @Override
+    public PageResponseDTO<AllContractDTO> getContractListWithPaging(ContractSearchDTO criteria) {
+
+        // 데이터 조회
+        List<AllContractDTO> list = contractQueryMapper.getContractList(criteria);
+
+        // 전체 개수 조회
+        long totalCount = contractQueryMapper.getCountContract(criteria);
+
+        // 페이지 버튼 정보계산
+        PagingButtonInfo paging = Pagination.getPagingButtonInfo(criteria, totalCount);
+
+        return new PageResponseDTO<>(list, totalCount, paging);
+    }
+}
