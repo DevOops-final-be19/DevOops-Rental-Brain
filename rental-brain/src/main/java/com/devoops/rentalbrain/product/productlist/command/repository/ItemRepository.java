ea56f028831a2566cae08ba2e96e1c596854dbcb
Seojin Yun <seojin@Seojinui-MacBookPro.local>
2025-12-18 17:04:03 +0900

    제품 리팩토링

--- rental-brain/src/main/java/com/devoops/rentalbrain/product/productlist/command/repository/ItemRepository.java
+++ rental-brain/src/main/java/com/devoops/rentalbrain/product/productlist/command/repository/ItemRepository.java
@@ -3,5 +3,8 @@ package com.devoops.rentalbrain.product.productlist.command.repository;
 import com.devoops.rentalbrain.product.productlist.command.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByName(String name);
 }
